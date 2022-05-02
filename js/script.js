a41ff6e3a0e3d0cbe621dcbc347a50838e38a0e7
IvanRusso91 <iv.russo91@gmail.com>
2022-05-02 16:48:48 +0200

    inserita scritta con Vue

---
+++ js/script.js
@@ -0,0 +1,13 @@
+
+const app = new Vue({
+
+  el:'#app',
+
+  data:{
+    message: 'Hello World!',
+    myColor: 'secondaryColor',
+    myStyle: 'decoration',
+    myPosition: 'position',
+    myImage: 'img'
+  }
+})
